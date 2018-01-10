99d8f45292b45be050a9a273164514a9960e02b7
weichenyang1 <35290598+weichenyang1@users.noreply.github.com>
2018-01-10 18:19:20 +0800

    i第一次修改test
    对test的内容，加了3行的打印语句

--- test.java
+++ test.java
@@ -1,5 +1,8 @@
 public class test{
   public static void main(String[] args){
       System.out.println("aaaaaa");
+      System.out.println("aaaaaa");
+      System.out.println("aaaaaa");
+      System.out.println("aaaaaa");
   }
 }
